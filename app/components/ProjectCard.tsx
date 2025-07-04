eb24b4284d1da29c9a623184ca6c4e72120e5cbc
Ben Chang <benlchang32@gmail.com>
2025-07-04 17:24:18 -0400

    project card fade works

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -19,20 +19,19 @@ const ProjectCard: React.FC<cardProps> = ( {id, name, imgPath, desc, stack, link
     return (
         <div className='projcard'>
             <div className={`proj-wrapper ${id}`}>
-                <div className='proj-fade-wrapper'>
-                    <div className='proj-entry'>
-                        <h3 className='proj-name'>{name}</h3>
-                        <div> </div>
-                        <div className='proj-desc'>{desc}</div>
-                        <div>
-                            {stack.map(item => 
-                                <Image src={item.imgPath} className='lfCard small' alt={item.name}/>
-                            )}
-                        </div>
-                        <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <span>{link !== '/' && <Link className='proj-link' href={link}>About the Project →</Link>}</span> 
-                            <span>{github !== '/' && <Link className='proj-link' target='_blank' href={github}>GitHub →</Link>}</span>
-                        </div>
+                <div className='proj-fade-wrapper'></div>
+                <div className='proj-entry'>
+                    <h3 className='proj-name'>{name}</h3>
+                    <div> </div>
+                    <div className='proj-desc'>{desc}</div>
+                    <div>
+                        {stack.map(item => 
+                            <Image src={item.imgPath} className='lfCard small' alt={item.name}/>
+                        )}
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                        <span>{link !== '/' && <Link className='proj-link' href={link}>About the Project →</Link>}</span> 
+                        <span>{github !== '/' && <Link className='proj-link' target='_blank' href={github}>GitHub →</Link>}</span>
                     </div>
                 </div>
             </div>
